e3a224d52b14023791388cde91327a80f7ac6036
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-01-12 18:41:36 -0300

    clean project and nativewind config

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const App = () => {
+    return (
+        <View className=''>
+            <Text className='text-3xl font-bold text-blue-500'>Hola mundo</Text>
+        </View>
+    );
+};
+
+export default App;
