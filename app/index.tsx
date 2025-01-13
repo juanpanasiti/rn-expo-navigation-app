7639c91595757a95a728f960e1a9d18c21b6dfe6
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-01-13 01:40:49 -0300

    implements custom fonts configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
 
 const App = () => {
     return (
-        <View className=''>
-            <Text className='text-3xl font-bold text-blue-500'>Hola mundo</Text>
-        </View>
+        <SafeAreaView>
+            <View className='mt-6 mx-2.5'>
+                <Text className='text-3xl' style={{ fontFamily: 'WorkSans-Black' }}>
+                    Hola mundo
+                </Text>
+                <Text className='text-3xl font-work-black text-primary'>Hola mundo</Text>
+                <Text className='text-3xl font-work-light'>Hola mundo</Text>
+                <Text className='text-3xl font-work-medium text-primary'>Hola mundo</Text>
+            </View>
+        </SafeAreaView>
     );
 };
 
